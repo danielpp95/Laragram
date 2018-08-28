708938d402eda1e6848465ac237902b820e1eb79
Daniel Pereira <danielpp95@gmail.com>
2018-08-27 22:42:09 -0400

    post component remake

--- resources/views/components/post.blade.php
+++ resources/views/components/post.blade.php
@@ -1,9 +1,37 @@
-<div class=" navbar bg-primary col-md-4 mx-auto mb-4 p-0" >
-  <div class="">
-      <p class="card-title mt-2">
-        <img class="rounded-circle col-md-2" style="max-width: 13%" src="{{$post->user->avatar}}"  alt="profile picture" >
-        <strong> {{$post->user->username}} </strong>
-      </p>
+<div class="card  col-md-4 mx-auto mb-4 p-0" >
+  {{-- title bar --}}
+  <div class="navbar bg-primary my-0 py-0" style="height: 40px !important; min-height: 40px !important;" >
+    <p>
+      <img 
+        class="rounded-circle col-md-2 p-0" 
+        style="max-width: 7%" 
+        src="{{$post->user->avatar}}"  
+        alt="profile picture" >
+      {{$post->user->username}}
+    </p>
+  </div>
+
+  {{-- image --}}
+  <img class="card-img-bottom rounded-0" src={{$post->image}} alt="Card image cap"  >
+
+  {{-- Buttons --}}
+    <div class="mx-3 mt-2">
+      <a href="#" class="card-link"><i class="tim-icons icon-heart-2"></i></a>
+      <a href="#" class="card-link"><i class="tim-icons icon-pencil"></i></a>
+      <a href="#" class="card-link"><i class="tim-icons icon-tag"></i></a>
+    </div>
+    <div class="card-footer">
+      <p class="card-text"> <strong>{{$post->user->username}}</strong> {{$post->caption}}</p>
+    </div>
+    
+
+  {{-- <div class="">
+      <div class="navbar bg-primary my-0" style="min-height: 50px" >
+        <p class="py-0">
+          <img class="rounded-circle col-md-2 p-0" style="max-width: 7%" src="{{$post->user->avatar}}"  alt="profile picture" >
+          <strong> {{$post->user->username}} </strong>
+        </p>
+      </div>
     <img class="card-img-bottom rounded-0" src={{$post->image}} alt="Card image cap"  >
     <div class="mx-3 mb-2">
       <a href="#" class="card-link"><i class="tim-icons icon-heart-2"></i></a>
@@ -11,5 +39,5 @@
       <a href="#" class="card-link"><i class="tim-icons icon-tag"></i></a>
       <p class="card-text">{{$post->caption}}</p>
     </div>
-  </div>
+  </div> --}}
 </div>
