bcdd2bdb69a042090456863bf7c2affb30b08440
Daniel Pereira <danielpp95@gmail.com>
2018-08-27 22:42:22 -0400

    post component remake

--- resources/views/components/post.blade.php
+++ resources/views/components/post.blade.php
@@ -24,20 +24,4 @@
       <p class="card-text"> <strong>{{$post->user->username}}</strong> {{$post->caption}}</p>
     </div>
     
-
-  {{-- <div class="">
-      <div class="navbar bg-primary my-0" style="min-height: 50px" >
-        <p class="py-0">
-          <img class="rounded-circle col-md-2 p-0" style="max-width: 7%" src="{{$post->user->avatar}}"  alt="profile picture" >
-          <strong> {{$post->user->username}} </strong>
-        </p>
-      </div>
-    <img class="card-img-bottom rounded-0" src={{$post->image}} alt="Card image cap"  >
-    <div class="mx-3 mb-2">
-      <a href="#" class="card-link"><i class="tim-icons icon-heart-2"></i></a>
-      <a href="#" class="card-link"><i class="tim-icons icon-pencil"></i></a>
-      <a href="#" class="card-link"><i class="tim-icons icon-tag"></i></a>
-      <p class="card-text">{{$post->caption}}</p>
-    </div>
-  </div> --}}
 </div>
