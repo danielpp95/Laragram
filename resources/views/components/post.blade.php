d89bbd8330815cb352fc0d44cb86042f7b3732b9
Daniel Pereira <danielpp95@gmail.com>
2018-08-27 21:57:55 -0400

    posr has user

--- resources/views/components/post.blade.php
+++ resources/views/components/post.blade.php
@@ -1,11 +1,15 @@
-<div class="ml-auto mr-auto card col-md-4 " >
-  <div class="card-body">
-    <h4 class="card-title">Card title</h4>
-    <img class="card-img-bottom" src={{$post->image}} alt="Card image cap"  >
-    <br>
-    <a href="#" class="card-link"><i class="tim-icons icon-heart-2"></i></a>
-    <a href="#" class="card-link"><i class="tim-icons icon-pencil"></i></a>
-    <a href="#" class="card-link"><i class="tim-icons icon-tag"></i></a>
-    <p class="card-text">{{$post->caption}}</p>
+<div class=" navbar bg-primary col-md-4 mx-auto mb-4 p-0" >
+  <div class="">
+      <p class="card-title mt-2">
+        <img class="rounded-circle col-md-2" style="max-width: 13%" src="{{$post->user->avatar}}"  alt="profile picture" >
+        <strong> {{$post->user->username}} </strong>
+      </p>
+    <img class="card-img-bottom rounded-0" src={{$post->image}} alt="Card image cap"  >
+    <div class="mx-3 mb-2">
+      <a href="#" class="card-link"><i class="tim-icons icon-heart-2"></i></a>
+      <a href="#" class="card-link"><i class="tim-icons icon-pencil"></i></a>
+      <a href="#" class="card-link"><i class="tim-icons icon-tag"></i></a>
+      <p class="card-text">{{$post->caption}}</p>
+    </div>
   </div>
 </div>
